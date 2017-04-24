eb2fa44e02f2c0a861a3553786a9c1c77c0436f6
Saad Ullah <saad.ullah@ozitechnology.com>
2017-04-24 15:07:33 +0500

    Added Submodule and dependent files

---
+++ TelloTalkV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelloTalkV2
+//
+//  Created by saad ullah on 20/04/2017.
+//  Copyright © 2017 saad ullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TelloTalkV2Tests/TelloTalkV2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TelloTalkV2Tests.swift
+//  TelloTalkV2Tests
+//
+//  Created by saad ullah on 20/04/2017.
+//  Copyright © 2017 saad ullah. All rights reserved.
+//
+
+import XCTest
+@testable import TelloTalkV2
+
+class TelloTalkV2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
